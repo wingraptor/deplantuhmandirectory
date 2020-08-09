20f645a6578592c26b94abfb8f9a5489cc70d902
Akono Brathwaite <akonobrathwaite@gmail.com>
2020-08-09 09:17:38 -0400

    Change jumbotron image and text

--- src/components/Jumbotron.vue
+++ src/components/Jumbotron.vue
@@ -1,21 +1,34 @@
 <template>
-  <div class="jumbotron-component">
-    <div class="test">
+  <div class="jumbotron-component w-100 d-flex align-items-center">
+    <!-- <div class="test">
       <b-img src="https://i.imgur.com/6fp8Iks.jpg" fluid-grow alt="Responsive image"></b-img>
+      <h4 class="w-50 position-absolute text-danger mt-4 mr-5 pr-5">This is also<br>available on your mobile.</h4>
+    </div>-->
+    <div class="text-white ml-5">
+      <h1>De Plantuh Man Directory</h1>
+      <p id="page-description" class="mt-5">Scroll to see a selection of agricultural products, services and contacts available in Barbados.</p>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: "Jumbotron"
+  name: "Jumbotron",
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
 .jumbotron-component {
   margin-bottom: 1.5rem;
+  background: linear-gradient(rgba(0, 0, 0, 0.164), rgba(0, 0, 0, 0.5)), url("https://i.imgur.com/yS0q2tz.jpg");
+  background-position: center;
+  background-size: cover;
+  height: 500px;
+  font-family: 'Roboto Slab', serif;
+}
+
+#page-description{
+  font-size: 1.5rem;
 }
 </style>
